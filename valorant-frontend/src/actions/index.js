997b78e265f6042e9b8e0b558c7e9e82599e0b41
adarvin14 <amdarvin14@gmail.com>
2021-07-03 13:21:48 -0400

    add actions

---
+++ valorant-frontend/src/actions/index.js
@@ -0,0 +1,110 @@
+export const getReviews = () => {
+    return dispatch => {
+        dispatch({ type: "LOADING" })
+        fetch("http://localhost:3001/reviews")
+            .then(resp => resp.json())
+            .then(reviews => dispatch({ type: "SET_REVIEWS", reviews }))
+    }
+}
+
+export const addReview = (review, history) => {
+    return dispatch => {
+        fetch('http://localhost:3001/reviews', {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ ...review, album_id: review.title })
+        })
+            .then(resp => resp.json())
+            .then(review => {
+                console.log(review);
+                dispatch({ type: "ADD_REVIEW", review })
+                history.push("/reviews")
+            }).catch(err => {
+                console.log('err occurred==>>', err);
+            })
+    }
+}
+
+export const deleteReview = (review, history) => {
+    return dispatch => {
+        fetch('http://localhost:3001/reviews', {
+            method: "DELETE",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ review })
+        })
+            .then(resp => resp.json())
+            .then(review => {
+                dispatch({ type: "DELETE_REVIEWS", review })
+                history.push("/reviews")
+            })
+    }
+}
+
+// export const editReview = (review, history) => {
+//     return dispatch => {
+//         fetch('http://localhost:3001/reviews', {
+//             method: "UPDATE",
+//             headers: {
+//             "Accept": "application/json",
+//             "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({ review })
+//         })
+//             .then(resp => resp.json())
+//             .then(review => {
+//                 dispatch({ type: "EDIT_REVIEW", review })
+//                 history.push("/reviews")
+//             })
+//     }
+// }
+
+export const getAlbums = () => {
+    return dispatch => {
+        dispatch({ type: "LOADING" })
+        fetch("http://localhost:3001/albums")
+            .then(resp => resp.json())
+            .then(albums => dispatch({ type: "SET_ALBUMS", albums }))
+    }
+}
+
+export const addAlbum = (album, history) => {
+    return dispatch => {
+        fetch('http://localhost:3001/albums', {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ album })
+        })
+            .then(resp => resp.json())
+            .then(album => {
+                dispatch({ type: "ADD_ALBUM", album })
+                history.push("/albums")
+            })
+    }
+}
+
+export const deleteAlbum = (album, history) => {
+    return dispatch => {
+        fetch('http://localhost:3001/albums', {
+            method: "DELETE",
+            headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ album })
+        })
+            .then(resp => resp.json())
+            .then(album => {
+                dispatch({ type: "DELETE_ALBUMS", album })
+                history.push("/albums")
+            })
+    }
+}
