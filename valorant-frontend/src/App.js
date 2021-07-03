315552048d0125aac207379325eaa9e32ec3eed3
adarvin14 <amdarvin14@gmail.com>
2021-07-03 13:11:09 -0400

    add form page, update app.js for new components

--- valorant-frontend/src/App.js
+++ valorant-frontend/src/App.js
@@ -4,6 +4,10 @@ import Nav from './components/Nav';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Contact from './components/Contact'
+import Footer from './components/Footer';
+import Albums from './components/Albums';
+import AlbumForm from './components/AlbumForm';
+import ErrorPage from './components/Error';
 
 function App() {
   return (
@@ -13,7 +17,14 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/' exact component={Contact} />
+          <Route path="/about" component={ About } />
+          <Route exact path="/albums" component={ Albums } />
+          <Route exact path="/albums/new" component={ AlbumForm } />
+          <Route exact path="/reviews" component={ Reviews } />
+          <Route exact path="/reviews/new" component={ ReviewForm } />
+          <Route component={ErrorPage} />
         </Switch>
+        <Footer />
       </Router>
     </>
   );
